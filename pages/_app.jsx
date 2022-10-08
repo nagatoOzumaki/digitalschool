52441ac681fe8003651426a49ee11dda242d9738
riad <riad@gmail.com>
2022-10-08 05:02:21 -0400

    ++

---
+++ pages/_app.jsx
@@ -0,0 +1,28 @@
+import '../styles/globals.css';
+// import type { AppProps } from 'next/app';
+// function MyApp({ Component, pageProps }: AppProps) {
+
+function MyApp({ Component, pageProps }) {
+  if(typeof(window)===undefined )
+  {(function (h, o, t, j, a, r) {
+    h.hj =
+      h.hj ||
+      function () {
+        (h.hj.q = h.hj.q || []).push(arguments);
+      };
+    h._hjSettings = { hjid: 3189697, hjsv: 6 };
+    a = o.getElementsByTagName('head')[0];
+    r = o.createElement('script');
+    r.async = 1;
+    r.src = t + h._hjSettings.hjid + j + h._hjSettings.hjsv;
+    a.appendChild(r);
+  })(window, document, 'https://static.hotjar.com/c/hotjar-', '.js?sv=');
+}
+  return (
+    <>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
