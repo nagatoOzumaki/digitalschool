0fddd7cfb7986598351ccc578c354790e76cd1d9
riad <riad@gmail.com>
2022-10-08 06:12:25 -0400

    ++

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -3,20 +3,27 @@ import '../styles/globals.css';
 // function MyApp({ Component, pageProps }: AppProps) {
 
 function MyApp({ Component, pageProps }) {
-  if(typeof(window)===undefined )
-  {
-
-      (function(h,o,t,j,a,r){
-          h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-          h._hjSettings={hjid:3192874,hjsv:6};
-          a=o.getElementsByTagName('head')[0];
-          r=o.createElement('script');r.async=1;
-          r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-          a.appendChild(r);
-      })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-  }
+  
   return (
     <>
+     <head>   
+      
+      {typeof(window)===undefined ?<script>
+   {
+   
+   (function(h,o,t,j,a,r){
+       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
+       h._hjSettings={hjid:3192874,hjsv:6};
+       a=o.getElementsByTagName('head')[0];
+       r=o.createElement('script');r.async=1;
+       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
+       a.appendChild(r);
+   }
+   )(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
+                     };
+   
+         </script>:null}
+           </head>
       <Component {...pageProps} />
     </>
   );
