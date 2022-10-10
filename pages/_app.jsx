ca4d6eb246fd503b1c74b3cace5e8151429c523c
riad <riad@gmail.com>
2022-10-10 06:23:59 -0400

    ++

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,29 +1,15 @@
+import { useEffect } from 'react';
 import '../styles/globals.css';
-// import type { AppProps } from 'next/app';
-// function MyApp({ Component, pageProps }: AppProps) {
+import { hotjar } from 'react-hotjar'
+
 
 function MyApp({ Component, pageProps }) {
-  
+  useEffect(() => {
+    hotjar.initialize(3189697,6)
+  }, [])
   return (
     <>
-     <head>   
-      
-      {typeof(window)===undefined ?<script>
-   {
-   
-   (function(h,o,t,j,a,r){
-       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-       h._hjSettings={hjid:3192874,hjsv:6};
-       a=o.getElementsByTagName('head')[0];
-       r=o.createElement('script');r.async=1;
-       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-       a.appendChild(r);
-   }
-   )(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')
-                     };
-   
-         </script>:null}
-           </head>
+  
       <Component {...pageProps} />
     </>
   );
