c7a031cdb91068959b1575258e47d8c422b18c17
riad <riad@gmail.com>
2022-10-10 06:37:35 -0400

    ++

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,15 +1,29 @@
 import { useEffect } from 'react';
 import '../styles/globals.css';
-import { hotjar } from 'react-hotjar'
-
+import { hotjar } from 'react-hotjar';
+import { Head } from 'next/document';
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
-    hotjar.initialize(3189697,6)
-  }, [])
+    hotjar.initialize(3189697, 6);
+  }, []);
   return (
     <>
-  
+      <Head>
+        <script
+          async
+          src='https://www.googletagmanager.com/gtag/js?id=TRACKING-ID'
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+   window.dataLayer = window.dataLayer || [];
+   function gtag(){dataLayer.push(arguments);}
+   gtag('js', new Date());
+   gtag('config', 'TRACKING-ID');`,
+          }}
+        ></script>
+      </Head>
       <Component {...pageProps} />
     </>
   );
